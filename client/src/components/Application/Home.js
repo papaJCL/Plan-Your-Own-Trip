0ca13ecc464b6981ef4fd16e20bd5a88d6997298
papaJCL <jeremylessermore@gmail.com>
2019-02-06 16:09:47 -0700

    Cleaned code for Home.js

--- client/src/components/Application/Home.js
+++ client/src/components/Application/Home.js
@@ -11,69 +11,60 @@ import Pane from './Pane'
  */
 export default class Home extends Component {
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col xs={12} sm={12} md={7} lg={8} xl={9}>
-            {this.renderMap()}
-          </Col>
-          <Col xs={12} sm={12} md={5} lg={4} xl={3}>
-            {this.renderIntro()}
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col xs={12} sm={12} md={7} lg={8} xl={9}>
+                        {this.renderMap()}
+                    </Col>
+                    <Col xs={12} sm={12} md={5} lg={4} xl={3}>
+                        {this.renderIntro()}
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 
-  renderMap() {
-    return (
-      <Pane header={'Where Am I?'}
-            bodyJSX={this.renderLeafletMap()}/>
-    );
-  }
+    renderMap() {
+        return (
+            <Pane header={'Where Am I?'}
+            bodyJSX={this.renderLeafletMap()}
+            />
+        );
+    }
 
-  renderLeafletMap() {
-    // initial map placement can use either of these approaches:
-    // 1: bounds={this.coloradoGeographicBoundaries()}
-    // 2: center={this.csuOvalGeographicCoordinates()} zoom={10}
-    return (
-      <Map center={this.csuOvalGeographicCoordinates()} zoom={10}
-           style={{height: 500, maxwidth: 700}}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                   attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        <Marker position={this.csuOvalGeographicCoordinates()}
-                icon={this.markerIcon()}>
-          <Popup className="font-weight-extrabold">Colorado State University</Popup>
-        </Marker>
-      </Map>
-    )
-  }
+    renderLeafletMap() {
+        return (
+            <Map center={[40.576179, -105.080773]} zoom={10}
+            style={{height: 500, maxwidth: 700}}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+                <Marker position={[40.576179, -105.080773]}
+                    icon={this.markerIcon()}>
+                    <Popup className="font-weight-extrabold">Colorado State University</Popup>
+                </Marker>
+            </Map>
+        );
+    }
 
-  renderIntro() {
-    return(
-      <Pane header={'Bon Voyage!'}
-            bodyJSX={'Let us help you plan your next trip.'}/>
-    );
-  }
+    renderIntro() {
+        return(
+            <Pane header={'Bon Voyage!'}
+            bodyJSX={'Let us help you plan your next trip.'}
+            />
+        );
+    }
 
-  coloradoGeographicBoundaries() {
-    // northwest and southeast corners of the state of Colorado
-    return L.latLngBounds(L.latLng(41, -109), L.latLng(37, -102));
-  }
-
-  csuOvalGeographicCoordinates() {
-    return L.latLng(40.576179, -105.080773);
-  }
-
-  markerIcon() {
-    // react-leaflet does not currently handle default marker icons correctly,
-    // so we must create our own
-    return L.icon({
-      iconUrl: icon,
-      shadowUrl: iconShadow,
-      iconAnchor: [12,40]  // for proper placement
-    })
-  }
+    markerIcon() {
+        // react-leaflet does not currently handle default marker icons correctly,
+        // so we must create our own
+        return L.icon({
+            iconUrl: icon,
+            shadowUrl: iconShadow,
+            iconAnchor: [12,40]  // for proper placement
+        })
+    }
 }
