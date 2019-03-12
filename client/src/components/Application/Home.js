573228f334c11bfab5f67266f9af5bfc92ca9d2f
Darien <dariencupit1@gmail.com>
2019-03-12 17:03:30 -0600

    Added delete button to popup

--- client/src/components/Application/Home.js
+++ client/src/components/Application/Home.js
@@ -289,7 +289,7 @@ export default class Home extends Component {
                         {
                             this.props.markers.map((position, idx) =>
                                 <Marker key={`marker-${idx}`} position={position} icon={this.markerIcon()}>
-                                    <Popup><div>Location {idx + 1}<br /><button onClick={() => this.changeStartLocation(idx)}>Make Origin</button></div></Popup>
+                                    <Popup><div>Location {idx + 1}<br /><button onClick={() => this.changeStartLocation(idx)}>Make Origin</button><button onClick={() => this.deleteLocation(idx)}>Delete</button><br /></div></Popup>
                                 </Marker>
                             )}
 
