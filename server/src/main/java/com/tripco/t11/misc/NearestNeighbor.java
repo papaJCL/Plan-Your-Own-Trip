1b3b04a30bc0f8decb73d6a4e06b56b4f734707e
Edward Pike <epike919@rams.colostate.edu>
2019-03-14 02:25:05 -0600

    built Optimizations class to be completed later and NearestNeighbor

---
+++ server/src/main/java/com/tripco/t11/misc/NearestNeighbor.java
@@ -0,0 +1,15 @@
+package com.tripco.t11.misc;
+
+
+
+public class NearestNeighbor extends Optimizations {
+
+    public int[] trip;
+    public int[] currentTrip;
+    private Long bestDist;
+    private Long[][] distances;
+    private boolean[] visited;
+
+
+
+}
