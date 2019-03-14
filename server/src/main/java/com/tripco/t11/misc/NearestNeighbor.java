12e6c651b7a75b71850acc0b199e8fd4966d5f12
Edward Pike <epike919@rams.colostate.edu>
2019-03-14 02:26:27 -0600

    built NearestNeighbor constructor

--- server/src/main/java/com/tripco/t11/misc/NearestNeighbor.java
+++ server/src/main/java/com/tripco/t11/misc/NearestNeighbor.java
@@ -10,6 +10,14 @@ public class NearestNeighbor extends Optimizations {
     private Long[][] distances;
     private boolean[] visited;
 
+    public NearestNeighbor(Double[][] coords, Double earthRadius){
+        this.trip = new int[coords.length];
+        this.currentTrip = new int[coords.length];
+        //generateDistances(coords, earthRadius);
+        this.visited = new boolean[coords.length];
+        this.bestDist = Long.MAX_VALUE;
+    }
+
 
 
 }
