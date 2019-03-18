ddc49cbd7d6b55974deca5fdaa1c08f31f5e5e6d
papaJCL <jeremylessermore@gmail.com>
2019-03-17 19:53:06 -0600

    Added the mapItinerary.js file to clean code

--- client/src/components/Application/Home.js
+++ client/src/components/Application/Home.js
@@ -8,6 +8,7 @@ import Pane from './Pane'
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle } from 'reactstrap';
 import Itinerary from './Itinerary'
+import MapItinerary from './mapItinerary'
 
 
 import { getOriginalServerPort, sendServerRequestWithBody } from '../../api/restfulAPI'
@@ -23,118 +24,55 @@ export default class Home extends Component {
 
     constructor(props) {
         super(props)
-        this.onChange = this.onChange.bind(this);
         this.sendItineraryRequest = this.sendItineraryRequest.bind(this)
-        this.reRenderNewMap = this.reRenderNewMap.bind(this)
-        this.clearMap = this.clearMap.bind(this)
-        this.download = this.download.bind(this)
-        this.createUploadButton = this.createUploadButton.bind(this)
-        this.createResetButton = this.createResetButton.bind(this)
-        this.createDownloadButton = this.createDownloadButton.bind(this)
-        this.changeStartLocation = this.changeStartLocation.bind(this)
         this.deleteLocation = this.deleteLocation.bind(this)
+        this.changeStartLocation = this.changeStartLocation.bind(this)
+        this.reRenderNewMap = this.reRenderNewMap.bind(this)
+    }
+
+    callMapItinerary(){
+        return (
+            <MapItinerary
+                clientSettings = {this.props.clientSettings}
+                clearMapState = {this.props.clearMapState}
+                reRenderNewMapState = {this.props.reRenderNewMapState}
+                markers = {this.props.markers}
+                JSONString = {this.props.JSONString}
+                returnFile = {this.props.returnFile}
+                latitude = {this.props.latitude}
+                longitude = {this.props.longitude}
+                boolMarker = {this.props.boolMarker}
+                polyLineCoor = {this.props.polyLineCoor}
+                names = {this.props.names}
+                liftHomeState = {this.props.liftHomeState}
+                updatePlacesArray = {this.props.updatePlacesArray}
+                deleteError = {this.props.deleteError}
+                deleteLocation = {this.deleteLocation}
+                sendItineraryRequest = {this.sendItineraryRequest}
+                ref="child"
+            />
+        )
+    }
+
+    callItinerary(){
+        return (
+            <Itinerary
+                boolMarker = {this.props.boolMarker}
+                JSONString = {this.props.JSONString}
+                changeStartLocation = {this.changeStartLocation}
+                deleteLocation = {this.deleteLocation}
+            />
+        )
     }
 
     render() {
         return (
             <Container>
-                <Row>
-                    <Col xs={12} sm={12} md={7} lg={8} xl={9}>
-                        {this.renderMap()}
-                    </Col>
-                    <Col xs={12} sm={12} md={5} lg={4} xl={3}>
-                        {this.renderIntro()}
-
-                    </Col>
-                </Row>
-                <Itinerary
-                    boolMarker = {this.props.boolMarker}
-                    JSONString = {this.props.JSONString}
-                    changeStartLocation = {this.changeStartLocation}
-                    deleteLocation = {this.deleteLocation}
-                />
+                {this.callMapItinerary()}
+                {this.callItinerary()}
             </Container>
         );
     }
-
-    /*download(jsonData, 'json.txt', 'text/plain');*/
-    /*Modified code beloning to Rafał Łużyński on www.stackoverflow.com*/
-    download() {
-        var content = JSON.stringify(this.props.returnFile);
-        var fileName = 'my Trip';
-        var contentType = 'text/plain';
-        var a = document.createElement("a");
-        var file = new Blob([content], {type: contentType});
-        a.href = URL.createObjectURL(file);
-        a.download = fileName;
-        a.click();
-    }
-
-
-    renderIntro(){
-        return(
-            <Pane header={'Itinerary Menu'}
-                 bodyJSX={
-                    <div>
-
-                        <row>
-                         {'Choose your file'}
-                        </row>
-                        <span>
-
-                         {this.createUploadButton()}
-
-                         {this.createResetButton()}
-
-                         {this.createDownloadButton()}
-
-                        </span>
-                    </div>}
-        />
-    );
-    }
-
-
-    createDownloadButton(){
-        return(
-            <Card>
-                <CardBody>
-                     <CardTitle><b>Download Trip Itinerary</b></CardTitle>
-                    <row>
-                        <Button onClick={this.download}>Download Trip Itinerary</Button>
-                    </row>
-
-                </CardBody>
-            </Card>
-        );
-    }
-
-
-    createUploadButton(){
-        return(
-            <Card>
-                <CardBody>
-                    <CardTitle><b>Upload Itinerary</b></CardTitle>
-                    <input type="file"name="myFile" onChange={this.onChange}/>
-                </CardBody>
-            </Card>
-             );
-
-    }
-
-    createResetButton(){
-        return(
-            <Card>
-                <CardBody>
-                    <CardTitle><b>Reset Map</b></CardTitle>
-                    <row>
-                        <Button onClick={this.clearMap}>Reset Map to default</Button>
-                    </row>
-                </CardBody>
-             </Card>
-    );
-    }
-
 
     changeStartLocation(idx) {
         let places = this.props.JSONString.body.places;
@@ -156,8 +94,12 @@ export default class Home extends Component {
     }
 
 
-    clearMap(){
-        this.props.clearMapState();
+    sendItineraryRequest(requestBody) {
+
+        sendServerRequestWithBody('itinerary', requestBody, this.props.clientSettings.serverPort)
+            .then((response) => {
+                this.props.liftHomeState(response);
+            });
     }
 
     reRenderNewMap(){
@@ -187,94 +129,4 @@ export default class Home extends Component {
         this.props.reRenderNewMapState(latitude, longitude, names, polyLine, markers)
     }
 
-    onChange(event) {
-        var file = event.target.files[0];
-        var reader = new FileReader();
-        reader.onload = (event) =>  {
-            // The file's text will be printed here
-            var inputData = event.target.result
-            this.sendItineraryRequest(JSON.parse(inputData))
-
-        };
-        reader.readAsText(file);
-        event.target.value = null;
-    }
-
-    sendItineraryRequest(requestBody) {
-
-        sendServerRequestWithBody('itinerary', requestBody, this.props.clientSettings.serverPort)
-            .then((response) => {
-                this.props.liftHomeState(response);
-            });
-    }
-
-    renderMap() {
-        return (
-            <Pane header={'World Map'}
-                  bodyJSX={this.renderLeafletMap()}
-            />
-        );
-    }
-
-    renderBasicMap(){
-        return(
-            <div>
-                <Map center={[0,0]} zoom={2}
-                     style={{height: 500, maxwidth: 700}}>
-                    <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                    />
-                </Map>
-            </div>
-        )
-    }
-
-    renderComplexMap(){
-        return (
-            <div>
-                <Map bounds = {this.props.markers} animate = {true}
-                     style={{height: 500, maxwidth: 700}}>
-                    <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                    />
-                    <Polyline
-                        positions ={this.props.polyLineCoor}
-                        color = {'black'}
-                        weight = {5}
-                        opacity = {0.5}
-                        smoothFactor = {1}
-                    />
-                    {
-                        this.props.markers.map((position, idx) =>
-                            <Marker key={`marker-${idx}`} position={position} icon={this.markerIcon()}>
-                                <Popup><div align="center"><b>Location {idx + 1}</b><br /><button onClick={() => this.changeStartLocation(idx)}>Make Origin</button><br /><button onClick={() => this.deleteLocation(idx)}>Delete</button><br /></div></Popup>
-                            </Marker>
-                        )}
-
-                </Map>
-            </div>
-
-        );
-    }
-
-    renderLeafletMap() {
-        if (this.props.boolMarker == false) {
-            return ( this.renderBasicMap());
-        }
-        else {
-            return (this.renderComplexMap());
-        }
-    }
-
-    markerIcon() {
-        // react-leaflet does not currently handle default marker icons correctly,
-        // so we must create our own
-        return L.icon({
-            iconUrl: icon,
-            shadowUrl: iconShadow,
-            iconAnchor: [12,40]  // for proper placement
-        })
-    }
 }
