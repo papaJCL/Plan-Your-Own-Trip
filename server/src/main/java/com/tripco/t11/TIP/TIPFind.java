33889d6f7d2e1bf6375f99b4535213e360cec799
Edward Pike <epike919@rams.colostate.edu>
2019-03-25 22:28:00 -0600

    built function to create match query

--- server/src/main/java/com/tripco/t11/TIP/TIPFind.java
+++ server/src/main/java/com/tripco/t11/TIP/TIPFind.java
@@ -34,16 +34,17 @@ public class TIPFind extends TIPHeader {
         TIPConfig config = new TIPConfig();
         config.buildResponse();
         String countQuery = buildCountQuery();
-        //String matchQuery = buildMatchQuery(config.placeAttributes);
+        String matchQuery = buildMatchQuery(config.placeAttributes);
         try {
             Class.forName(myDriver);
             try (Connection connect = DriverManager.getConnection(myUrl, user, pass);
                  Statement stCount = connect.createStatement();
-                 //Statement stQuery = connect.createStatement();
+                 Statement stQuery = connect.createStatement();
                  ResultSet rsCount = stCount.executeQuery(countQuery);
-                 //ResultSet rsQuery = stQuery.executeQuery(matchQuery);
+                 ResultSet rsQuery = stQuery.executeQuery(matchQuery);
             )   {
-
+                rsCount.next();
+                this.found = rsCount.getInt(1);
             }
         } catch (Exception e) {
             System.err.println("Exception: " + e.getMessage());
@@ -55,8 +56,20 @@ public class TIPFind extends TIPHeader {
         return countQuery;
     }
 
-    private String buildMatchQuery(){
-        return "";
+    private String buildMatchQuery(List<String> attributes){
+        String matchQuery = "select ";
+        for(int i = 0; i < attributes.size() - 1; ++i){
+            matchQuery += attributes.get(i) + ",";
+        }
+        matchQuery += attributes.get(attributes.size() - 1) + " ";
+        matchQuery += queryEnd();
+        if(limit != null){
+            if(limit != 0){
+                matchQuery += " limit " + Integer.toString(limit);
+            }
+        }
+        matchQuery += ";";
+        return matchQuery;
     }
 
     private String queryEnd(){
