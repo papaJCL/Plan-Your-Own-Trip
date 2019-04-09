5121c0f128ae31977d265b3f2c57d5f3f135790a
Jeremy Lesser <jeremylessermore@gmail.com>
2019-04-08 20:18:16 -0600

    Completely revamped SQL to be a standalone object(WIP)

--- client/src/components/Application/SQL.js
+++ client/src/components/Application/SQL.js
@@ -1,191 +1,143 @@
 import React, {Component} from 'react';
 import {Container, Row, Col, Button} from 'reactstrap';
 import 'leaflet/dist/leaflet.css';
-import Pane from './Pane'
+import Pane from './Pane';
+import { Map, Marker, Popup, TileLayer, Polyline} from 'react-leaflet';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle , Table} from 'reactstrap';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import { Form, Label, Input  } from 'reactstrap';
 import BootstrapTable1 from 'react-bootstrap-table-next';
-import ToolkitProvider, { ColumnToggle } from 'react-bootstrap-table2-toolkit';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/leaflet.css';
 import { sendServerRequestWithBody } from '../../api/restfulAPI'
 
 
 export default class SQL extends Component {
     constructor(props) {
         super(props)
-        this.SQLMenu = this.SQLMenu.bind(this);
-        //this.returnSQLItinerary = this.returnSQLItinerary.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    render() { console.log('I am in render for SQL.js')
+    render() {
         return (
             <div>
-                {this.SQLMenu()}
+            <Pane header = {'SQL markers will load here'}
+                  bodyJSX={[this.createSearchBar() , this.renderLeafletMap()]}
+            />
+            <Pane header = {'The table will load here'}/>
             </div>
-        )
-
-    }
-
-    varBody(){ console.log('I am in varBody')
-        let work = this.props.SQLJson.places
-        var body = work.map((item, idx) =>
-            <tr>
-                <td> {idx + 1} </td>
-                <td> {item.id} </td>
-                <td> {item.name} </td>
-                <td> {item.altitude} </td>
-                <td> {item.latitude} </td>
-                <td> {item.longitude} </td>
-                <td> {item.municipality} </td>
-                <td> {this.buttonSQL(idx)} </td>
-            </tr>
-        )
-        return(body)
-    }
-
-    SQLBody(){ console.log('I am in SQLBody --- SQLJSON: ', this.props.SQLJson)
-        if (this.props.SQLJson.places == null) return;
-        else{ return( this.varBody());
-        }
-    }
-
-    buttonSQL(idx){ console.log('I am in buttonSQL')
-        let work = this.props.SQLJson.places[idx]
-        return(
-            <button onClick={() => this.callNewItineraryWithSQL(work) }>Add to Itinerary</button>
         );
     }
 
-    SQLTable() {
-        return (
-            <Table>
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Altitude</th>
-                    <th>Latitude</th>
-                    <th>Longitude</th>
-                    <th>Municipality</th>
-                    <th>Add to Itinerary</th>
-                </tr>
-                </thead>
-                <tbody>
-                {this.SQLBody()}
-                </tbody>
-            </Table>
-        )
+    handleSubmit(e) {
+        let location = document.getElementById('location').value;
+        let region = document.getElementById('region').value;
+        let country = document.getElementById('country').value;
+        let continent = document.getElementById('continent').value;
+
+        var request = {
+            'requestType':'find',
+            'requestVersion': 3,
+            'match': location,
+            'limit': 5
+        };
+        sendServerRequestWithBody('find',request,this.props.clientSettings.serverPort)
+            .then((response) => {
+                console.log(response.body)
+                this.props.updateSQLState(response.body);
+            });
+        e.preventDefault();
+
     }
-    SQLMenu(){
+
+    createSearchBar(){
         return(
-            <row>
-                <Pane header = {'SQL Header'}
-                      bodyJSX ={
-                          <div>
-                              {this.SQLTable()}
-                          </div>
-                      }
-                />
-            </row>
+                <CardBody>
+                    <CardTitle><b>Search for Destination</b></CardTitle>
+                    <row>
+                        <form onSubmit={this.handleSubmit}>
+                            <label>
+                                <input id="location" type="text" placeholder="Enter Location"  />
+                                <input id="region" type="text" placeholder="Enter Region"  />
+                                <input id="country" type="text" placeholder="Enter Country"  />
+                                <input id="continent" type="text" placeholder="Enter Continent"  />
+                                <input id="name" type="submit" value="Submit"/>
+                            </label>
+                        </form>
+                    </row>
+                </CardBody>
+        );
+    }
+
+
+
+
+
+
+    renderLeafletMap() {
+        if (this.props.boolMarker == false) {
+            return ( this.renderBasicMap());
+        }
+        else {
+            return (this.renderComplexMap());
+        }
+    }
+
+
+    renderBasicMap(){
+        return(
+            <div>
+                <Map center={[0,0]} zoom={2}
+                     style={{height: 500, maxwidth: 700}}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    />
+                </Map>
+            </div>
         )
     }
 
-    callNewItineraryWithSQL(work){ console.log('button pressed: callNewItineraryWithSQL - work: ', work)
-        //this.props.addLocation(work.name, work.latitude, work.longitude);
-        return (this.props.updateItinerarySQL(work));
+
+    renderComplexMap(){
+        return (
+            <div>
+                <Map bounds = {this.props.markers} animate = {true}
+                     style={{height: 500, maxwidth: 700}}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    />
+                    <Polyline
+                        positions ={this.props.polyLineCoor}
+                        color = {'black'}
+                        weight = {5}
+                        opacity = {0.5}
+                        smoothFactor = {1}
+                    />
+                    {
+                        this.props.markers.map((position, idx) =>
+                            <Marker key={`marker-${idx}`} position={position} icon={this.markerIcon()}>
+                                <Popup><div align="center"><b>Location {idx + 1}</b><br /><button onClick={() => this.props.changeStartLocation(idx)}>Make Origin</button><br /><button onClick={() => this.props.deleteLocation(idx)}>Delete</button><br /></div></Popup>
+                            </Marker>
+                        )}
+
+                </Map>
+            </div>
+
+        );
     }
 
-/*
-        finalizeSQLItinerary(){ console.log('finalizeSQLItinerary')
-            return (
-                <button onClick={() => this.sendSQLRequest()}>Click this when SQL Itinerary is done</button>
-            );
-        }
-
-        sendSQLRequest(){
-            console.log("sendSQLRequest")
-            console.log(this.props.JSONString.body)
-            console.log(this.props.planOptions.activeUnit)
-            var request = {
-                "requestType"    : "itinerary",
-                "requestVersion" : 3,
-                "options"        : {"earthRadius": "" + Math.round(parseFloat(this.props.JSONString.body.options.earthRadius))},
-                "places"         : this.props.SQLItineraryInfo,
-                "distances"      : []
-            };
-            sendServerRequestWithBody('itinerary',request,this.props.clientSettings.serverPort)
-                .then((response) => {
-                    console.log(response.body)
-                    this.props.liftHomeState(response);
-                    this.props.boolSQLFunc();
-                });
-
-        }
-
-        SQLProducts(){ console.log('SQLProducts: ', this.props.SQLItineraryInfo)
-            const products = [];
-            const startId = products.length;
-            for (let i = 0; i < this.props.SQLItineraryInfo.length; i++) {
-                const id = startId + i;
-                products[i] = ({
-                    id: id + 1,
-                    name: this.props.SQLItineraryInfo[i].name,
-                    latitude: this.props.SQLItineraryInfo[i].latitude ,
-                    longitude: this.props.SQLItineraryInfo[i].longitude,
-                    municipality: this.props.SQLItineraryInfo[i].municipality
-                });
-            }
-            return products
-        }
-
-        SQLColumns(){
-            var columns = [{
-                dataField: 'id',
-                text: 'ID',
-
-            },{
-                dataField: 'name',
-                text: 'Name',
-
-            },{
-                dataField: 'latitude',
-                text: 'latitude',
-
-            }, {
-                dataField: 'longitude',
-                text: 'Longitude',
-            },{
-                dataField: 'municipality',
-                text: 'Municipality'
-            }];
-            return columns
-        }
-
-        returnSQLItinerary(){
-            var products = this.SQLProducts();
-            var cols = this.SQLColumns();
-            console.log('products; ', products)
-            return (
-                <div>
-                    <Pane
-                        header={this.finalizeSQLItinerary()}
-                        bodyJSX={
-                            <BootstrapTable1
-                                selectRow={{mode: 'checkbox'}}
-                                tabIndexCell
-                                bootstrap4
-                                keyField="id"
-                                data={products}
-                                columns={cols}>
-                            </BootstrapTable1>
-                        }
-                    />
-                </div>
-            );
-        }
-    */
-
+    markerIcon() {
+        // react-leaflet does not currently handle default marker icons correctly,
+        // so we must create our own
+        return L.icon({
+            iconUrl: icon,
+            shadowUrl: iconShadow,
+            iconAnchor: [12,40]  // for proper placement
+        })
+    }
 
 }
