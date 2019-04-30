2534dcf99ad09a1102719cb93159a736061ebb70
Darien <dariencupit1@gmail.com>
2019-04-30 13:31:33 -0600

    Added alert input handling to switch order box

--- client/src/components/Application/Itinerary.js
+++ client/src/components/Application/Itinerary.js
@@ -194,6 +194,11 @@ export default class Iitnerary extends Component {
         let handleSubmit = (event) => {
             event.preventDefault();
             let number = document.getElementById(columnIndex);
+            if (number.value === "" || number.value > this.props.JSONString.body.places.length) {
+                alert("Please enter a valid integer from " + 1 + " to " + this.props.JSONString.body.places.length);
+                number.focus();
+                return;
+            }
             this.props.changeOrder(columnIndex, number.value - 1);
         };
 
