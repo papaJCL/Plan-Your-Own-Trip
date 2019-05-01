b7003a5db81d32f993140293a2dacb9282d1bf6c
Jeremy Lesser <jeremylessermore@gmail.com>
2019-04-30 21:33:55 -0600

    Updated SQL.test to support removal of map

--- client/test/SQL.test.js
+++ client/test/SQL.test.js
@@ -53,12 +53,9 @@ function testBasic(){
     //    let milesNotOrigional = testConvert.instance().convertDistance(10, 'miles', 'kilometers');
     basicSQLTest.instance().sendSQLRequest()
     basicSQLTest.update();
-    basicSQLTest.instance().renderBasicMap();
-    basicSQLTest.instance().renderCondensedMap();
     //basicSQLTest.setProps({ JSONString: startProperties.SQLItineraryInfo.concat(startProperties.JSONString) });
 
     basicSQLTest.instance().renderSQLTable();
-    basicSQLTest.instance().renderTableB4Itinerary();
 
     expect(basicSQLTest.props().JSONString).toEqual(startProperties.JSONString)
 
@@ -66,8 +63,7 @@ function testBasic(){
     expect(sanitizeTest).toEqual("xd5a")
 
     basicSQLTest.instance().SQLColumns;
-    basicSQLTest.instance().callNewMapState(55, 43);
-    basicSQLTest.instance().returnSQLItinerary();
+
 
 
 
