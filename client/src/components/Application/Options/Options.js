afbf3fbb345f20d2181e82f2be8074e2a4e8514a
Darien <dariencupit1@gmail.com>
2019-05-08 21:48:29 -0600

    Got rid of commented out code in options.js

--- client/src/components/Application/Options/Options.js
+++ client/src/components/Application/Options/Options.js
@@ -38,12 +38,6 @@ export default class Options extends Component{
                      updateOldUnit = {this.props.updateOldUnit}
                      updateOption={this.props.updateOption}/>
             </Col>
-
-              {/* Insert here <- different column, same row.*/}
-              {/*<Col xs="12" sm="12" md="6" lg="4" xl="3">
-                <CustomUnits/>
-            </Col> */}
-
           </Row>
             <Row>
             <Col xs="12" sm="12" md="6" lg="4" xl="3">
